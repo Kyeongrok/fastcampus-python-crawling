dac403270e3e95ed68225529317155d83a88a86f
Kyeongrok Kim <oceanfog1@gmail.com>
2022-08-05 15:21:22 +0900

    video comment

--- a_naver_finance/video02/__init__.py
+++
@@ -1,5 +0,0 @@
-# api프로젝트 만들고
-# api프로젝트에서 youtube data api
-import googleapiclient
-
-
---
+++ d_youtube/01_video_comment.py
@@ -0,0 +1,33 @@
+# api프로젝트 만들고
+# api프로젝트에서 youtube data api
+
+from googleapiclient.discovery import build
+import os
+
+api_key = os.getenv("YOUTUBE_API_KEY")
+print(api_key)
+video_id = 'exgO1LFl9x8'
+
+comments = list()
+api_obj = build('youtube', 'v3', developerKey=api_key)
+response = api_obj.commentThreads().list(part='snippet,replies', videoId=video_id, maxResults=100).execute()
+
+while response:
+    for item in response['items']:
+        comment = item['snippet']['topLevelComment']['snippet']
+        comments.append(
+            [comment['textDisplay'], comment['authorDisplayName'], comment['publishedAt'], comment['likeCount']])
+
+        if item['snippet']['totalReplyCount'] > 0:
+            for reply_item in item['replies']['comments']:
+                reply = reply_item['snippet']
+                comments.append(
+                    [reply['textDisplay'], reply['authorDisplayName'], reply['publishedAt'], reply['likeCount']])
+
+    if 'nextPageToken' in response:
+        response = api_obj.commentThreads().list(part='snippet,replies', videoId=video_id,
+                                                 pageToken=response['nextPageToken'], maxResults=100).execute()
+    else:
+        break
+
+print(comments)
