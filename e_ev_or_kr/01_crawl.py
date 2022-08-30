a3c1dcb07f63a333cc94c41b0adceec93ff9c5cb
Kyeongrok <oceanfog1@gmail.com>
2022-08-30 09:49:22 +0900

    dir정리

--- e_ev_or_kr/01_crawl.py
+++
@@ -1,6 +0,0 @@
-import requests
-from bs4 import BeautifulSoup
-
-data = requests.get("https://www.ev.or.kr/portal/localInfo")
-file = open("ev_or_kr_local_info.html", "w+")
-file.write(data.text)
--- e_ev_or_kr/06_make_library.py
+++
@@ -1,4 +0,0 @@
-
-# Library에 Class를 저장
-# Import
-# crawl과 연결
